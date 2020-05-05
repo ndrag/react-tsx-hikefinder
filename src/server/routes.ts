4ab01aee5b278293f436c3ac6415e007e1c18129
Nick Dragunow <ndraguno@gmail.com>
2020-05-05 19:21:31 +1200

    Express - trust Heroku API proxy.

--- src/server/routes.ts
+++ src/server/routes.ts
@@ -4,11 +4,10 @@ import DB from './db';
 
 const router = express.Router();
 
-router.use(function(req, res, next) {
-
-  res.header("Access-Control-Allow-Origin", `${req.protocol}://ndragunow.nz`); // TODO: Move to a config file.
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
+router.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", `${req.protocol}://ndragunow.nz`); // TODO: Move to a config file.
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
 });
 
 router.get('/api/hello', (req, res, next) => {
--- src/server/server.ts
+++ src/server/server.ts
@@ -4,6 +4,7 @@ import apiRouter from './routes';
 const app = express();
 
 app.use(express.static('public'));
+app.enable('trust proxy');
 app.use(apiRouter);
 
 const port = process.env.PORT || 3000;
